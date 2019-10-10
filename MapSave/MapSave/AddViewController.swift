d4f5e1315ecea19b1c763bb969e2ad5f795ae2aa
haruka <>
2019-10-10 11:52:57 +0900

    情報登録可能 ボタン制御追加

--- MapSave/MapSave/AddViewController.swift
+++ MapSave/MapSave/AddViewController.swift
@@ -19,6 +19,14 @@ class AddViewController: UIViewController, UITextFieldDelegate {
     // 登録ボタン
     @IBOutlet weak var inputButton: UIButton!
     
+    // 名前と住所の変数
+    var nameBox:String?
+    var addressBox:String?
+    var resultText:String?
+    
+    // 名前と住所の配列
+    var addressArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -26,19 +34,67 @@ class AddViewController: UIViewController, UITextFieldDelegate {
         nameField.delegate = self
         addressField.delegate = self
         
+        // ボタン制御
+        inputButton.isEnabled = false
+        inputButton.backgroundColor = UIColor.gray
+        
     }
     
     // 登録ボタンが押されたときの処理
     @IBAction func inputAction(_ sender: UIButton) {
+        // 名前と住所を変数に格納
+        nameBox = nameField.text ?? ""
+        addressBox = addressField.text ?? ""
+        // 1つの変数にまとめる
+        resultText = nameBox! + "\n" + addressBox!
+        
+        // 配列を呼び出す
+        if UserDefaults.standard.object(forKey: "place") != nil {
+            addressArray = UserDefaults.standard.object(forKey: "place") as! [String]
+        }
+        // 配列にセット
+        addressArray.append(resultText!)
+        // 保存
+        UserDefaults.standard.set(addressArray, forKey: "place")
+        
+        // 初期化
+        nameField.text = ""
+        addressField.text = ""
+        
+        // 確認ダイアログ
+        let alert = UIAlertController(title: "確認", message: "登録完了！", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+        
+        print(addressArray)
+        
     }
     
     // リターンキーでキーボードを下ろす
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
+        // ボタンを活性化する
+        if nameField.text != "" && addressField.text != "" {
+            inputButton.isEnabled = true
+            inputButton.backgroundColor = UIColor.blue
+        } else {
+            // ボタン制御
+            inputButton.isEnabled = false
+            inputButton.backgroundColor = UIColor.gray
+        }
         return true
     }
     // 画面外タッチでキーボードを下ろす
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // ボタンを活性化する
+        if nameField.text != "" && addressField.text != "" {
+            inputButton.isEnabled = true
+            inputButton.backgroundColor = UIColor.blue
+        } else {
+            // ボタン制御
+            inputButton.isEnabled = false
+            inputButton.backgroundColor = UIColor.gray
+        }
         self.view.endEditing(true)
     }
 }
---
+++ MapSave/MapSave/ViewControlleanir.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  MapSave
+//
+//  Created by haruka on 2019/10/09.
+//  Copyright © 2019 haruka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var addressTable: UITableView!
+    
+    var addressArray = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // デリゲート
+        addressTable.delegate = self
+        addressTable.dataSource = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // 保存した配列がnilでなければ呼び出し
+        if UserDefaults.standard.object(forKey: "place") != nil {
+            addressArray = UserDefaults.standard.object(forKey: "place") as! [String]
+        }
+        // リロード
+        addressTable.reloadData()
+        
+    }
+    
+    // セクションの中のセルの数
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return addressArray.count
+    }
+    // セルの中身
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // どのセルか認識させる
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        // 複数行に対応させる
+        cell.textLabel?.numberOfLines = 0
+        // セルのテキスト
+        cell.textLabel?.text = addressArray[indexPath.row]
+        
+        return cell
+    }
+
+    // セルをスライドした時の処理
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // 配列から情報を消す
+            addressArray.remove(at: indexPath.row)
+            // 消した後の配列を改めて保存
+            UserDefaults.standard.set(addressArray, forKey: "place")
+            // リロード
+            addressTable.reloadData()
+        }
+    }
+
+
+}
+
